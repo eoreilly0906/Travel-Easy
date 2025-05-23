f48eeee1f8ecb868bc435448a0639049a124fc38
Edward O <edwardoreilly0906@gmail.com>
2025-05-22 21:19:11 -0500

    Remove unused Link import from Login component

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,4 @@
 import { useState, type FormEvent, type ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
