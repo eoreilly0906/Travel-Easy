8fe8d4529388b6011c26e4574db0f426b2e52f62
Edward O <edwardoreilly0906@gmail.com>
2025-05-22 21:52:27 -0500

    Update GraphQL endpoint configuration in App.tsx for production and development environments
    - Modified the GraphQL API endpoint to switch between production and local URLs based on the NODE_ENV variable.
    - Ensured seamless integration for both development and production setups.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,7 +13,9 @@ import Footer from './components/Footer';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: process.env.NODE_ENV === 'production' 
+    ? 'https://travel-easy-21g7.onrender.com/graphql'
+    : 'http://localhost:3001/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
